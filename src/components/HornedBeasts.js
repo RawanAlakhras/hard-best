693465d9ecca2107121e5c9324d2d6778b8974ff
RawanAlakhras <ralakhras36@gmail.com>
2021-06-08 17:25:17 +0300

    complete lab03

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-
+import {Modal,Button} from 'react-bootstrap/'
 class HornedBeasts extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            numberClick: 0
+            numberClick: 0,
+            show:false,
+
         }
+    }
+
+    handelClick = ()=>{
+        this.setState({
+            show:true,
+        })
+    }
+    handleClose =()=>{
+        this.setState({
+            show:false,
+        })
     }
     onClickHundel = () => {
         this.setState({
@@ -25,7 +38,7 @@ class HornedBeasts extends React.Component {
             // </div>
 
             <div>
-                <Card style={{ width: '18rem' }}>
+                <Card style={{ width: '18rem' }} onClick={this.handelClick}>
                     <Card.Img variant="top" onClick={this.onClickHundel} src={this.props.imageUrl} alt={this.props.title} />
                     <Card.Body>
                         <Card.Title>{this.props.title}</Card.Title>
@@ -38,6 +51,21 @@ class HornedBeasts extends React.Component {
 
                     </Card.Body>
                 </Card>
+
+                <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+          <Modal.Title>{this.props.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>{this.props.description}
+          <img src={this.props.imageUrl} alt={this.props.title} className='card-img-top'></img>
+          </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={this.handleClose}>
+            Close
+          </Button>
+         
+        </Modal.Footer>
+      </Modal>
             </div>
         )
     }
