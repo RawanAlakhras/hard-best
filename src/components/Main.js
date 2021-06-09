94ef9529989a3fef776f82b25069666c2a53bbf6
RawanAlakhras <ralakhras36@gmail.com>
2021-06-09 20:47:34 +0300

    complet lab-04

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,42 +1,73 @@
 import React from 'react';
 import HornedBeasts from './HornedBeasts';
-import infoData from  './data.json';
-import Row from 'react-bootstrap/Row';
+import infoData from './data.json';
+import { Row, Form } from 'react-bootstrap/';
 
-class Main extends React.Component{
-    constructor(props){
+class Main extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={
-            show:false,
-
+        this.state = {
+           
+            horns:'all',
+            arr:infoData,
         }
+        this.filter = this.filter.bind(this);
     }
+    filter (event) {
+        event.preventDefault();
+        
+        this.setState({
+            horns:event.target.value,
+        }) 
+        if(event.target.value != 'all'){
+            this.setState({
+                arr:infoData.filter(a => a.horns == event.target.value)
+            })
+           
+        }else{
+            this.setState({
+                arr:infoData,
+            })
+        }
 
-    render(){
-        return(
-           <>
-            <div className='container' >
-                {/* <HornedBeasts title={'Rawan'} imageUrl={'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'} description={'Mother (or father) rhino with two babies'}/>
-                <HornedBeasts title={'Rawan'} imageUrl={'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'} description={'Mother (or father) rhino with two babies'}/> */}
-                <Row xs={1} md={3} className="g-4">
-                    {infoData.map((item,idx) => 
-                        
-                           <HornedBeasts onClick={this.handelClick}
-                                title= {item.title}
+       
+    }
+    render() {
+        return (
+            <>
+
+                <div className='container' >
+                
+                    <Form >
+                        <Form.Group controlId="exampleForm.SelectCustom">
+                            <Form.Label>how many horns?</Form.Label>
+                            <Form.Control as="select" custom name='select1' onChange={this.filter}>
+                               <option value='all'>all</option>
+                                <option value='1'>one</option>
+                                <option value='2'>two</option>
+                                <option value='3'>three</option>
+                                <option value='100'>Wow..</option> 
+                            </Form.Control>
+                        </Form.Group>
+                    </Form>
+                  
+                    <Row xs={1} md={3} className="g-4">
+                        {this.state.arr.map((item, idx) => 
+                            <HornedBeasts
+                                title={item.title}
                                 imageUrl={item.image_url}
-                                description = {item.description}
+                                description={item.description}
                                 key={idx.toString()}
 
-                           />
+                            />
                         
-                    
-                    )}
-                </Row>
-                
-     
-            </div>
-           </>
-            
+                        )}
+                    </Row>
+
+
+                </div>
+            </>
+
         )
     }
 }
